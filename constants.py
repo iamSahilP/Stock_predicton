b59f1e9a295149374621126938a7a8e4d5540093
Sahil Patel <135223603+iamSahilP@users.noreply.github.com>
2024-06-03 11:12:11 +0530

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,14 @@
+# consumer_key= '7x7w8Ti8GmMtuhvj7IXBuvZmP'
+consumer_key='hPEWB6qYCyOGhDEQ3v37bRBYu'
+
+# consumer_secret= 'kpFGcKiGxP9dJtlQzZ99p2OK9HM0IjA1I8n23N40VJyctOzvkG'
+consumer_secret= '1IOd9qeNDjYGHhIWSIUDtrb1xkIqBY2CG50NnvzaLvEciTcwSd'
+
+
+
+# access_token='1365544389498474497-97lzyZE9lroLNDzLhvW1dLS3MDKfV5'
+access_token='843753783406530563-1pOu96FerHiRq6pYdr8ZrPdKfyPiz4X'
+# access_token_secret='8mF9CrBt4SvmewPZaapgL7kkh2iJus824SvhBJOvDpXlw'
+access_token_secret='FoxZb5BTUxzxWKgucEyTkT0Qgb8FM23BRAj3dpjXP0o6j'
+
+num_of_tweets = int(300)
